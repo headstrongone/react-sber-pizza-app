04b4eb5ee87520b28d6484a05c74748fca98020a
headstrongone <sneqqprojects@gmail.com>
2020-12-23 23:41:36 +0300

    added axios for getting pizza database instead of common js fetch

--- src/App.js
+++ src/App.js
@@ -3,15 +3,21 @@ import { Header } from './comp';
 import { Home, Cart } from './routing';
 import { Route } from 'react-router-dom';
 import { useState, useEffect } from 'react';
+import axios from "axios";
 
 function App() {
     const [state, setState] = useState([]);
     useEffect(() => {
-        fetch('http://localhost:3004/database.json')
-            .then((response) => response.json())
-            .then((json) => {
-                setState(json.pizzaList);
-            });
+        axios.get('http://localhost:3004/database.json').then(({ data } ) => {
+            setState(data.pizzaList)
+        });
+
+        // fetch('http://localhost:3004/database.json')
+        //     .then((response) => response.json())
+        //     .then((json) => {
+        //         setState(json.pizzaList);
+        //     });
+
     }, []);
 
     return (
